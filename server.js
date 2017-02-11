c9b6ebbe390575175fe6b9aa9a64361880fc7f73
Tim <sh.kang@ecubelabs.com>
2017-02-11 16:43:57 +0900

    자잘한것

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 var path = require('path');
 
 
-app.use('/static', express.static(__dirname + '/build/static'))
+app.use('/static', express.static(__dirname + '/build/static'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html');
-})
+});
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+    console.log('Example app listening on port 8080!');
+});
